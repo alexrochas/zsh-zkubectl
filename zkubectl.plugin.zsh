eafc163c2b032e5e5a59d40877456cb64400ef8c
Alex Rocha <arocha@zalando-61672.fritz.box>
2019-10-19 19:44:53 +0200

    refactor: change autocomplete strategy

--- zkubectl.plugin.zsh
+++ zkubectl.plugin.zsh
@@ -1,15 +1,1 @@
-
-function _zkubectl_login {
-    local line
-
-    case $words[2] in
-        login)
-            ctx=$(export KUBECTX_IGNORE_FZF=1; kubectx | fzf)
-
-            compadd $ctx
-        ;;
-    esac
-}
-
-compdef _zkubectl_login zkubectl
-
+fpath+="$(dirname $0)/src"
